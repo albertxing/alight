e2d7487f473272083c858207f26a2a1dd6b1f25d
Albert Xing <albertxing@users.noreply.github.com>
2016-01-03 11:07:32 -0800

    url param in js

--- inject.js
+++ inject.js
@@ -2,7 +2,7 @@
 	var req = new XMLHttpRequest();
 	req.open("post", "http://192.168.1.200:8000/");
 	req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	req.send("action=enter&referrer=" + document.referrer);
+	req.send("action=enter&url=" + window.location + "&referrer=" + document.referrer);
 	window.onbeforeunload = function () {
 		req = new XMLHttpRequest();
 		req.open("post", "http://192.168.1.200:8000", true);
